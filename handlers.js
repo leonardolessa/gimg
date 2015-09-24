091460c9519a312876b55712f9aeaad42df94048
Leonardo <lrlessa@gmail.com>
2015-09-24 19:43:41 -0300

    improve code

--- handlers.js
+++ handlers.js
@@ -1,4 +1,3 @@
-var gm = require('gm');
 var util = require('./util');
 
 require('bufferjs');
@@ -7,27 +6,14 @@ module.exports = {
 	index: function (req, reply) {
 		var path = util.random(req.params.type);
 
-		var streamImage = function(err, stdout, stderr) {
-			var base = [];
-
-			stdout.on('data', function(data) {
-				base.push(data);
-			});
-
-			stdout.on('close', function() {
-				var img = Buffer.concat(base);
-
-				reply(img)
-					.type('image/png')
-					.header('Content-Length', img.length);
-			});
-		};
-		
-		gm(path)
-			.resize(req.params.width, req.params.height, '^')
-			.gravity('Center')
-			.crop(req.params.width, req.params.height)
-			.stream(streamImage);
-
+		util.convert({
+			width: req.params.width,
+			height: req.params.height,
+			type: req.params.type
+		}, function(img) {
+			reply(img)
+				.type('image/png')
+				.header('Content-Length', img.length);
+		});
 	}
 };
--- util.js
+++ util.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var gm = require('gm');
 
 module.exports = {
 	random: function(type) {
@@ -7,5 +8,27 @@ module.exports = {
 		var randomNumber = Math.floor(Math.random() * (fileList.length - 1 + 1)) + 0;
 
 		return imagesPath + '/' + fileList[randomNumber];	
+	},
+
+	convert: function(data, cb) {
+		var path = this.random(data.type);
+
+		var streamImage = function(err, stdout, stderr) {
+			var base = [];
+
+			stdout.on('data', function(data) {
+				base.push(data);
+			});
+
+			stdout.on('close', function() {
+				cb(Buffer.concat(base));
+			});
+		};
+		
+		gm(path)
+			.resize(data.width, data.height, '^')
+			.gravity('Center')
+			.crop(data.width, data.height)
+			.stream(streamImage);
 	}
 };
